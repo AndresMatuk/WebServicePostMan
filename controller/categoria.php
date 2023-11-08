09f91781a1afc4f06c3dec058086dbba76376aa4
Andres Camilo Matuk <andres.matuk@soy.sena.edu.co>
2023-11-08 12:28:36 -0500

    Post con Select inner join where Id Y post de Insert

--- controller/categoria.php
+++ controller/categoria.php
@@ -3,15 +3,23 @@
     require_once("../models/Categoria.php");
     $categoria = new Categoria();
 
-    
+    $body = json_decode(file_get_contents("php://input"), true);
 
     switch($_GET["op"]){
         case "GetAll":
             $datos=$categoria->get_categoria();
             echo json_encode($datos);
         break;
+
+        case "GetId":
+            $datos=$categoria->get_Categoria_x_id($body["id_usuario"]);
+            echo json_encode($datos);
+        break;
+
+        case "insert":
+            $datos=$categoria->insert_Categoria($body["Fk_Id_Usuario"],$body["fecha_horan"],$body["Valor_consignación"]);
+            $mostrar_datos=$categoria->get_Categoria_x_id($body["Fk_Id_Usuario"]);
+            echo json_encode($mostrar_datos);
+        break;
     }
-
-
-
 ?>
--- models/Categoria.php
+++ models/Categoria.php
@@ -3,10 +3,32 @@
         public function get_categoria(){
             $conectar= parent::conexion();
             parent::set_names();
-            $sql="SELECT * FROM `usuarios`";
+            $sql="SELECT * FROM `consignaciones`";
             $sql=$conectar->prepare($sql);
             $sql->execute();
             return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
         }
+   
+        public function get_Categoria_x_id($id_usuario){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT usuarios.nombre, usuarios.apellido, consignaciones.id_consignaciones, consignaciones.fecha_horan, consignaciones.Valor_consignación  FROM `consignaciones` inner join `usuarios` where usuarios.id_usuario = consignaciones.fk_id_usuario AND id_usuario = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $id_usuario);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+        public function insert_Categoria($id_usuario,$fecha_horan,$Valor){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO Consignaciones (Fk_Id_Usuario, fecha_horan, Valor_consignación) VALUES (?,?,?)";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $id_usuario);
+            $sql->bindValue(2, $fecha_horan);
+            $sql->bindValue(3, $Valor);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
     }
+//SELECT usuarios.nombre, usuarios.apellido, consignaciones.*  FROM `consignaciones` inner join `usuarios` where usuarios.id_usuario = consignaciones.fk_id_usuario AND id_usuario = ?
 ?>
